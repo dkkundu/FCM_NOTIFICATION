8437863b4ab0d9bbc3ae3f39c05a5077bfad9ba3
Dipto K Kundu <dkkundu00@gmail.com>
2022-04-07 00:32:38 +0600

    Add FCM SETTINGS admin

--- notification/admin.py
+++ notification/admin.py
@@ -7,8 +7,4 @@ class FCMSettingsAdmin(admin.ModelAdmin):
     list_display = ['version', 'is_active', 'api_key']
     list_display_links = ['version', 'is_active', 'api_key']
     search_fields = ['version', 'is_active', 'api_key']
-    readonly_fields = [
-        'api_key', 'auth_domain', 'database_URL', 'project_id',
-        'storage_bucket', 'messaging_sender_id', 'api_key', 'measurement_id', 'server_key', 'action_domain',
-        'icon_image'
-    ]
+    readonly_fields = ['last_updated']
